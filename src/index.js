e35c2fdec44f997879ef50798d7252bc05ca0cec
bryan kretz <kretzbryan@gmail.com>
2021-01-30 17:43:45 -0800

    mongodb configured

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 4000;
+const config = require('config');
+const mongoURI = config.mongoURI;
+
+mongoose.connect(mongoURI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+	console.log('Connected to mongodb');
+});
+
+mongoose.connection.error('error', (err) => {
+	console.error('Error connecting to mongodb', err);
+});
 
 app.get('/', (req, res) => {
 	res.send('Hi there!');
