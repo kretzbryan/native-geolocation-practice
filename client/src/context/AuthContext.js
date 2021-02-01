ceb4c017aade8a7c14ea1ec70ec4dc8196fca7ab
bryan kretz <kretzbryan@gmail.com>
2021-01-31 18:18:46 -0800

    axios installed, skeleton for signin, signup,signout actions created

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
 	const { type, payload } = action;
@@ -8,8 +9,32 @@ const authReducer = (state, action) => {
 	}
 };
 
+const signup = (dispatch) => {
+	return async ({ email, password }) => {
+		try {
+			const res = trackerApi.post('signup', { email, password });
+		} catch (err) {}
+	};
+};
+
+const signin = (dispatch) => {
+	return async ({ email, password }) => {
+		try {
+			let res;
+		} catch (err) {}
+	};
+};
+
+const signout = (dispatch) => {
+	return async ({ email, password }) => {
+		try {
+			let res;
+		} catch (err) {}
+	};
+};
+
 export const { Provider, Context } = createDataContext(
 	authReducer,
-	{},
+	{ signup, signin, signout },
 	{ signedIn: false }
 );
